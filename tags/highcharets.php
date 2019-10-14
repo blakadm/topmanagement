7af80dc92544128ccb86f57edee099a4d98d0abc
blakadm <blakadm@gmail.com>
2019-10-14 13:55:07 +0800

    tests

---
+++ tags/highcharets.php
@@ -0,0 +1,43 @@
+<?php
+
+/* 
+ * Project Name: onelab.gov.ph-Top_Management * 
+ * Copyright(C)2018 Department of Science & Technology -IX * 
+ * Developer: Eng'r Nolan F. Sunico  * 
+ * 05 3, 18 , 10:51:51 AM * 
+ * Module: highcharets * 
+ */
+
+$Series = [
+    [
+        'name' => 'RSTL',
+        'colorByPoint' => true,
+        'type' => 'pie',
+        'data' => [
+            ['name' => 'DOST-I', 'y' => 61.14, 'sliced' => true, 'selected' => true],
+            ['name' => 'DOST-II', 'y' => 230],
+            ['name' => 'DOST-III', 'y' => 130],
+            ['name' => 'DOST-IV', 'y' => 80],
+            ['name' => 'DOST-V', 'y' => 330],
+            ['name' => 'DOST-VI', 'y' => 630],
+            ['name' => 'DOST-VII', 'y' => 1230],
+            ['name' => 'DOST-VIII', 'y' => 210],
+            ['name' => 'DOST-IX', 'y' => 2230]
+        ]
+    ],
+    [
+        'name' => 'Transactions',
+        'type' => 'spline',
+        'data' => [
+            ['name' => 'DOST-I', 'y' => 120],
+            ['name' => 'DOST-II', 'y' => 120],
+            ['name' => 'DOST-III', 'y' => 130],
+            ['name' => 'DOST-IV', 'y' => 108],
+            ['name' => 'DOST-V', 'y' => 133],
+            ['name' => 'DOST-VI', 'y' => 163],
+            ['name' => 'DOST-VII', 'y' => 1712],
+            ['name' => 'DOST-VIII', 'y' => 1021],
+            ['name' => 'DOST-IX', 'y' => 2520]
+        ]
+    ]
+];
